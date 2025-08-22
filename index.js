d75c56ba0c72df4f5acb5cf058a98022c10a749c
mertbabspro <142105576+mertbabspro@users.noreply.github.com>
2025-08-22 19:37:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function createBot() {
     // 4️⃣ Her dakika /shard pay obbyzz 170
     setTimeout(() => {
       setInterval(() => {
-        bot.chat("/shard pay obbyzz 1")
+        bot.chat("/shard pay obbyzz 180")
         console.log("/shard pay obbyzz 1 komutu gönderildi ✅")
       }, 60000) // 60000ms = 1 dakika
     }, 15000) // Önce balance komutu gönderilsin
@@ -56,5 +56,6 @@ function createBot() {
 }
 
 createBot()
+
 
 
